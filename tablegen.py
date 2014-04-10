8a8cec974af8a78b38bd5de8809a6b12a0678166
Colin M. Strickland <cms@beatworm.co.uk>
2014-04-10 22:08:35 +0100

    [add] emo unicode codepoint table generator

---
+++ tablegen.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# get the unicode data .txt file from 
+# http://source.icu-project.org/repos/icu/icu/trunk/source/data/unidata/UnicodeData.txt
+
+# pass this file to this script as an argument
+
+import fileinput
+import re
+
+emoticon_match = re.compile('^1F[3456][0-9ABCDEF]{2}')
+
+
+for line in fileinput.input():
+    if emoticon_match.match(line) :
+        (cp,desc) = line.split(';')[0:2]
+        desc = desc.lower().replace(' ','-')
+        print '\t"{0}" : "\U{1:0>8}",'.format(desc,cp)
