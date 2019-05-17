63345a497f70b71ae0f411ff92e61cd4761a42d8
Pete Beardmore <pete.beardmore@msn.com>
2019-05-17 17:55:29 +0100

    [add] matchsets filtering
    -parse comma delimited string of either build-in matchset names
    which have a pre-defined regex associated or arbitrary strings to
    matched in a wildcard manner against either codepoint or description
    -use '0x' hex prefix to deduce field to search on
    -support '+' / '-' prefixes to allow negative matching / blacklist
    filtering

--- ucp.py
+++ ucp.py
@@ -12,23 +12,71 @@ import re
 
 cwd = os.path.dirname(sys.argv[0])
 url_default = "https://www.unicode.org/Public/UCD/latest/ucd/UnicodeData.txt"
-emoticon_match = re.compile('^1F[3456][0-9ABCDEF]{2}')
+map_expressions_builtin = {
+   '_builtin_best_': [0, re.compile('^1f[3-6][0-9a-f]{2}')],
+   '_builtin_full_': [0, re.compile('^1f[0-6][0-9a-f]{2}')]
+}
+map_expressions_negative = []
+map_expressions_positive = []
+
+
+def parse_matchsets(matchsets):
+    matchsets = matchsets.split(',')
+    for m in matchsets:
+        m = m.strip()
+        print("parsing matchset: %r" % (m), file=sys.stderr)
+        if m == "":
+            continue  # discard empty
+        map_expressions_target = map_expressions_positive
+        if m[0:1] == "-":
+            map_expressions_target = map_expressions_negative
+            m = m[1:]
+        elif m[0:1] == "+":
+            m = m[1:]
+        if m in map_expressions_builtin:
+            map_expressions_target\
+                .append([m, *map_expressions_builtin[m]])
+            continue
+        match_on = 0  # codepoint
+        if m[0:2] == '0x':
+            m = m[2:]
+        else:
+            match_on = 1  # description
+        map_expressions_target.append(
+            ['custom', match_on, re.compile(".*%s.*" % (re.escape(m)))])
 
 
 def generate(source):
     print("# generating codepoints map", file=sys.stderr)
-    print("src: %r" % ("[stdin]" if source == '-'
-                                 else source), file=sys.stderr)
+    print("src: %r" % ("[stdin]" if source == '-' else source),
+          file=sys.stderr)
 
-    map_count = 0
+    # build matches
+    matches = []
     for line in fileinput.input(source):
-        if emoticon_match.match(line) :
-            (cp,desc) = line.split(';')[0:2]
-            desc = desc.lower().replace(' ','-')
-            cp = cp.lower()
-            print('\t"{0}" : 0x{1},'.format(desc,cp))
-            map_count += 1
-    print("# mapped %d codepoints" % (map_count), file=sys.stderr)
+        (cp,desc) = line.split(';')[0:2]
+        desc = desc.lower().replace(' ','-')
+        cp = cp.lower()
+        match = 0
+        for _, match_on, rx in map_expressions_negative:
+            if (match_on == 0 and rx.match(cp)) or \
+               (match_on == 1 and rx.match(desc)):
+                match = 1
+                break
+        if match == 1:
+            continue  # negative match
+        for _, match_on, rx in map_expressions_positive:
+            if (match_on == 0 and rx.match(cp)) or \
+               (match_on == 1 and rx.match(desc)):
+                match = 1
+                break
+        if match == 1:
+            matches.append([cp, desc])
+    print("# mapped %d codepoints" % (len(matches)), file=sys.stderr)
+
+    # build project map file
+    for cp, desc in matches:
+        print('\t"{0}" : 0x{1},'.format(desc,cp))
 
 
 def download(url, target):
@@ -66,6 +114,11 @@ parser.add_argument(
     default=url_default,
     help="set URL for ICU codepoints data blob")
 parser.add_argument(
+    '-ms', '--matchsets', dest='matchsets', nargs='?',
+    default='_builtin_best_',
+    help="comma delimited list of pre-defined matchsets" +
+         " and/or arbitrary match strings")
+parser.add_argument(
     '-s', '--source', type=str, default='-',
     help="ICU codepoints data blob (default: stdin)")
 
@@ -73,11 +126,19 @@ if not sys.argv[1:]:
     parser.print_help(sys.stderr)
     exit(0)
 
+args = parser.parse_args()
+
 source = args.source
 if args.download:
     target = os.path.join(cwd, 'codepoints', 'unicode-latest.raw')
     download(args.url, target)
     source = target
 if args.generate:
+    if args.matchsets:
+        try:
+            parse_matchsets(args.matchsets)
+        except Exception as e:
+            print("error: %s" % (e), file=sys.stderr)
+            exit(1)
     sys.argv = [sys.argv[0]]
     generate(source)
