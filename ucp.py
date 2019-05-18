77aa0794824554aa766a304762a8320c9d1f8c34
Pete Beardmore <pete.beardmore@msn.com>
2019-05-18 18:22:09 +0100

    [add] support for all unicode versions
    -supports official emoji codepoint data files, including multi-
    codepoint sequence files, thus removing the ambiguity (for unicode
    versions >5) in classification of emoji or not
    -redux downloading based on upstream structure
    -adds '-t' / '--target' options to target a specific unicode version,
    defaulting to the latest
    -add set of file and version specific parsers to collate data to
    'unicode-`version`.raw' file for onward filtering

--- ucp.py
+++ ucp.py
@@ -7,17 +7,61 @@ try:
     import urllib.request as http  # python 3
 except ImportError:
     import urllib as http  # python 2
-import fileinput
 import re
+from collections import OrderedDict
+from functools import reduce
 
 cwd = os.path.dirname(sys.argv[0])
-url_default = "https://www.unicode.org/Public/UCD/latest/ucd/UnicodeData.txt"
+latest = 12
+# upstream data
+url_root_default = "https://www.unicode.org/Public/"
+versions = OrderedDict(
+    {"unicode-" + str(v): v for v in range(5, latest + 1)})
+version_default = "unicode-" + str(latest)
+versions_structure = {}
+fns = {"u": "UnicodeData.txt", "d": "emoji-data.txt",
+       "s": "emoji-sequences.txt", "z": "emoji-zwj-sequences.txt",
+       "v": "emoji-variation-sequences.txt"}
+parsers = {}
+parsers["u"] = {
+    "unicode-latest": [re.compile("^([^;]+);([^;]+).*$"), "cd"]}
+parsers["d"] = {
+    "unicode-6": [re.compile("^([^;]+[^ ])[ ]*;(?:[^;]+;){3}[^#]+#[^)]+\) (.*)$"), "cd"],
+    "unicode-latest": [re.compile("^([^ ]+)[^#]+#[^[]+\[([0-9]+)\].*[ ]{2,}(.+)$"), "cnd"]}
+parsers["s"] = {
+    "unicode-latest": [re.compile("^([^;]+[^ ])[ ]*;[^;]+;[ ]*([^#]+[^ ])[ ]*#[^[]+\[([0-9]+)\].*$"), "cdn"]}
+parsers["z"] = {
+    "unicode-latest": parsers['s']['unicode-latest'] }
+parsers["v"] = {
+    "unicode-latest": parsers['s']['unicode-latest'] }
+
 map_expressions_builtin = {
-   '_builtin_best_': [0, re.compile('^1f[3-6][0-9a-f]{2}')],
-   '_builtin_full_': [0, re.compile('^1f[0-6][0-9a-f]{2}')]
+   '_emoji_': [0, re.compile('^0x1f[3-6][0-9a-f]{2}$')],
+   '_emoji2_': [2, re.compile('^1$')]
 }
 map_expressions_negative = []
 map_expressions_positive = []
+
+
+def build_targets(root, remote=True):
+    sep = "/" if remote else os.sep
+    for n, v in versions.items():
+        versions_structure[n] = {n: []}
+        sub_path = str(v) + ".0.0/ucd" if remote else "upstream"
+        versions_structure[n][n] = [
+            ['u', sep.join([x.strip(sep) for x in \
+                      [root, sub_path, fns['u']]])]]
+    for uv, v, fs in zip(list(range(6, latest + 1)),
+                         list(range(1, 6)) + list(range(11, latest + 1)),
+                         ["d", *(["dsz"] * 3), *(["dszv"] * 3)]):
+        nuv = "unicode-" + str(uv)
+        nv = "emoji-" + str(v)
+        versions_structure[nuv][nv] = []
+        sub_path = "emoji/" + str(v) + ".0/" if remote else "upstream"
+        for f in list(fs):
+            versions_structure[nuv][nv].append(
+                [f, sep.join([x.strip(sep) for x in \
+                        [root, sub_path, fns[f]]])])
 
 
 def parse_matchsets(matchsets):
@@ -46,61 +90,160 @@ def parse_matchsets(matchsets):
             ['custom', match_on, re.compile(".*%s.*" % (re.escape(m)))])
 
 
-def generate(source):
+def generate(source, version):
     print("# generating codepoints map", file=sys.stderr)
     print("src: %r" % ("[stdin]" if source == '-' else source),
           file=sys.stderr)
 
+    mappings = {}
+    if os.path.isdir(source):
+        # parse sources
+        build_targets(source, False)
+
+        nuv = version
+        for [t, fp] in reduce(lambda l1, l2: l1 + l2,
+                              versions_structure[nuv].values()):
+            lines = []
+            with open(fp, 'r') as f:
+                lines = f.readlines()
+            print("processing %d lines of %r using parser type %r"
+                  % (len(lines), fp, t))
+            for line in [line.strip()
+                         for line in lines]:
+                # skip blanks and comments
+                if len(line) == 0 or line[0:1] == "#":
+                    continue
+
+                # parse with parser
+                p = parsers[t][nuv] if nuv in parsers[t] \
+                                    else parsers[t]["unicode-latest"]
+                rx, bits = p[0], list(p[1])
+                m = rx.match(line)
+                if not m:
+                    continue
+                c = m.groups()[bits.index('c')]
+                d = m.groups()[bits.index('d')]
+                n = 1
+                if 'n' in bits:
+                    n = int(m.groups()[bits.index('n')])
+
+                # cleanup
+                description = d.lower().replace(' ', '-')
+                if ' ' in c:
+                    codepoint = ' '.join(hex(int(x, 16))
+                                           for x in c.split(' '))
+                else:
+                    codepoint = hex(int(c.split('.')[0], 16))
+                # insert mapping(s)
+                if n == 1:
+                    mappings[codepoint] = \
+                       [description, 0 if t == 'u' else 1]
+                else:
+                    ci = int(codepoint, 16)
+                    for l in range(n):
+                        codepoint2 = hex(ci + l)
+                        description2 = mappings[codepoint2][0] \
+                            if codepoint2 in mappings \
+                            else description + " " + str(l)
+                        mappings[codepoint2] = [description2, 1]
+
+        # dump mappings
+        mappings = ["{0};{1};{2}".format(k, *v)
+                    for k, v in mappings.items()]
+        target = os.path.join(source, version + ".raw")
+        f = None
+        try:
+            f = open(target, 'w')
+            f.write('\n'.join(mappings))
+        except Exception:
+            print("cannot write to target file: %r" % (target),
+                  file=sys.stderr)
+            f.close()
+            exit(1)
+
+    else:
+        # read map file
+        with open(source, 'r') as f:
+            mappings = f.readlines()
+        source = os.path.dirname(source)
+
+    print("filtering %d codepoints" % (len(mappings)))
+
     # build matches
     matches = []
-    for line in fileinput.input(source):
-        (cp, desc) = line.split(';')[0:2]
-        desc = desc.lower().replace(' ', '-')
-        cp = cp.lower()
+    for parts in [line.split(';') for line in mappings]:
         match = 0
         for _, match_on, rx in map_expressions_negative:
-            if (match_on == 0 and rx.match(cp)) or \
-               (match_on == 1 and rx.match(desc)):
+            if rx.match(parts[match_on]):
                 match = 1
                 break
         if match == 1:
             continue  # negative match
         for _, match_on, rx in map_expressions_positive:
-            if (match_on == 0 and rx.match(cp)) or \
-               (match_on == 1 and rx.match(desc)):
+            if rx.match(parts[match_on]):
                 match = 1
                 break
         if match == 1:
-            matches.append([cp, desc])
-    print("# mapped %d codepoints" % (len(matches)), file=sys.stderr)
+            matches.append(";".join(parts[0:2]))
 
-    # build map file
-    for cp, desc in matches:
-        print('0x{0};{1}'.format(cp, desc))
+    print("mapped %d codepoints" % (len(matches)), file=sys.stderr)
 
-
-def download(url, target):
-    print("# downloading ICU codepoints", file=sys.stderr)
-
-    print("src: %r" % (url), file=sys.stderr)
-    print("dest: %r" % (target), file=sys.stderr)
-
+    # write map file
+    target = os.path.join(source, version + ".map")
     f = None
     try:
         f = open(target, 'w')
-        f.close()
+        f.write('\n'.join(matches))
     except Exception:
-        print("cannot write to target file: %r" % (target), file=sys.stderr)
-    try:
-        http.urlretrieve(url, target)
-    except Exception as e:
-        print("download failed:\n\n" + e, file=sys.stderr)
-        sys.exit(1)
+        print("cannot write to target file: %r" % (target),
+              file=sys.stderr)
+        f.close()
+        exit(1)
+
+
+def download(source, version, target):
+    print("# downloading ICU codepoints for %r" % (version),
+          file=sys.stderr)
+
+    target = os.path.join(target, "upstream")
+    if not os.path.isdir(target):
+        try:
+            os.makedirs(target)
+        except Exception:
+            print("cannot create target directory: %r" % (target),
+                  file=sys.stderr)
+            exit(1)
+
+    build_targets(source)
+    nuv = version
+    for [t, url] in reduce(lambda l1, l2: l1 + l2,
+                           versions_structure[nuv].values()):
+        dest = os.path.join(target, fns[t])
+        print("src: %r" % (url), file=sys.stderr)
+        print("dest: %r" % (dest), file=sys.stderr)
+
+        f = None
+        try:
+            f = open(dest, 'w')
+            f.close()
+        except Exception:
+            print("cannot write to target file: %r" % (dest),
+                  file=sys.stderr)
+            exit(1)
+        try:
+            http.urlretrieve(url, dest)
+        except Exception as e:
+            print("download failed:\n\n%s" % (e), file=sys.stderr)
+            sys.exit(1)
 
 
 parser = argparse.ArgumentParser(
     argument_default='-h',
     description='Unicode Codepoints Parser')
+parser.add_argument(
+    '-t', '--target', dest='target',
+    type=str, nargs='?', default=version_default,
+    help="unicode version to target")
 parser.add_argument(
     '-g', '--generate', action='store_const',
     const=True, default=False,
@@ -110,12 +253,12 @@ parser.add_argument(
     const=True, default=False,
     help="download latest ICU codepoints data blob")
 parser.add_argument(
-    '-u', '--url', dest='url', nargs='?',
-    default=url_default,
+    '-u', '--url', dest='url_root', nargs='?',
+    default=url_root_default,
     help="set URL for ICU codepoints data blob")
 parser.add_argument(
-    '-ms', '--matchsets', dest='matchsets', nargs='?',
-    default='_builtin_best_',
+    '-ms', '--matchsets', dest='matchsets',
+    default='_emoji2_',
     help="comma delimited list of pre-defined matchsets" +
          " and/or arbitrary match strings")
 parser.add_argument(
@@ -129,16 +272,16 @@ if not sys.argv[1:]:
 args = parser.parse_args()
 
 source = args.source
+version = args.target
 if args.download:
-    target = os.path.join(cwd, 'codepoints', 'unicode-latest.raw')
-    download(args.url, target)
+    target = os.path.join(cwd, 'codepoints', version)
+    download(args.url_root, version, target)
     source = target
 if args.generate:
-    if args.matchsets:
-        try:
-            parse_matchsets(args.matchsets)
-        except Exception as e:
-            print("error: %s" % (e), file=sys.stderr)
-            exit(1)
+    try:
+        parse_matchsets(args.matchsets)
+    except Exception as e:
+        print("error: %s" % (e), file=sys.stderr)
+        exit(1)
     sys.argv = [sys.argv[0]]
-    generate(source)
+    generate(source, version)
